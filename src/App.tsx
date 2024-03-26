cae7808445667368cdb19c95f507e30ff8efbb74
Sarthak <sarthakbhatt@live.com>
2024-03-27 03:12:35 +0530

    solved the assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import "./App.css";
-
+interface TodosProp {
+  title: string;
+  desc: string;
+}
 function App() {
+  const title = "Sarthak";
+  const desc = "Sarthak Todo";
   return (
     <div>
-      <Todos />
+      <Todos title={title} desc={desc} />
     </div>
   );
 }
 
 //create a component that takes todos as an input and renders them
 
-const Todos = () => {
-  return <div>Hello</div>;
+const Todos = (props: TodosProp) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.desc}</h2>
+    </div>
+  );
 };
 export default App;
