6ecbcbc8c038d73a7cf5f64ef96c176a69e0045c
Sarthak <sarthakbhatt@live.com>
2024-03-27 03:06:46 +0530

    created component Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
 }
 
+//create a component that takes todos as an input and renders them
+
+const Todos = () => {
+  return <div>Hello</div>;
+};
 export default App;
